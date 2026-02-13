dc2cec0b503a580ca625e2de3799101a23dd5ba0
insaf-chaouali <insafchaouali@gmail.com>
2026-02-13 14:03:42 +0100

    test

--- src/files/files.controller.spec.ts
+++ src/files/files.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FilesController } from './files.controller';
+import { FilesService } from './files.service';
 
 describe('FilesController', () => {
   let controller: FilesController;
@@ -7,6 +8,16 @@ describe('FilesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FilesController],
+      providers: [
+        {
+          provide: FilesService,
+          useValue: {
+            // Mock des méthodes appelées par le controller
+            saveFile: jest.fn(),
+            getFile: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<FilesController>(FilesController);
--- src/files/files.service.spec.ts
+++ src/files/files.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FilesService } from './files.service';
+import { MinioService } from '../storage/minio.service'; // Ajuste le chemin si besoin
 
 describe('FilesService', () => {
   let service: FilesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FilesService],
+      providers: [
+        FilesService,
+        {
+          provide: MinioService,
+          useValue: {
+            // Liste ici les méthodes que FilesService appelle, par exemple :
+            upload: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<FilesService>(FilesService);
